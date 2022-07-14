9d35f955d128632db8fdaf7ce2d4407937a5bf13
Marius <marius@example.com>
2022-07-14 16:35:49 +0300

    newPostObj form fields

--- js/posts.js
+++ js/posts.js
@@ -1,2 +1,23 @@
 'use strict';
 console.log('main.js');
+
+const createFormEl = document.forms[0];
+const titleEl = document.getElementById('title');
+const imageEl = document.getElementById('image');
+const dateEl = document.getElementById('date');
+const authorEl = document.getElementById('author');
+const bodyEl = document.getElementById('body');
+
+createFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  console.log('Js is in control');
+  const newPostObj = {
+    title: titleEl.value,
+    image: imageEl.value,
+    date: dateEl.value,
+    author: authorEl.value,
+    body: bodyEl.value,
+  };
+  console.log('newPostObj ===', newPostObj);
+});
