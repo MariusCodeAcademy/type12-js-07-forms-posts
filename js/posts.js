7b9b3e375594a6ee68a3f2aca0d559dcc136bea2
Marius <marius@example.com>
2022-07-14 17:02:22 +0300

    generate posts

--- js/posts.js
+++ js/posts.js
@@ -7,6 +7,7 @@ const imageEl = document.getElementById('image');
 const dateEl = document.getElementById('date');
 const authorEl = document.getElementById('author');
 const bodyEl = document.getElementById('body');
+const postsContainerEl = document.querySelector('.posts-container');
 
 createFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -20,4 +21,19 @@ createFormEl.addEventListener('submit', function (event) {
     body: bodyEl.value,
   };
   console.log('newPostObj ===', newPostObj);
+  makePostToHtml(newPostObj, postsContainerEl);
+  createFormEl.reset();
 });
+
+function makePostToHtml(post, dest) {
+  const artEl = document.createElement('div');
+  artEl.className = 'post card';
+  artEl.innerHTML = `
+    <img src="${post.image}" alt="paveikslelis">
+    <h3 class="post__title">${post.title}</h3>
+    <h4 class="post__author">Autorius: ${post.author}</h4>
+    <p class="post__date">${post.date}</p>
+    <p class="post__text">${post.body}</p>
+  `;
+  dest.append(artEl);
+}
